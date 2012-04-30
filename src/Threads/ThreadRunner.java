9c20ef86ebfcf25ef3ab1aa74c3c5d788858ce54
Tomas <Tomas@Tomas-PC>
2012-04-30 12:30:27 +0200

    initial commit

--- src/Threads/ThreadRunner.java
+++ src/Threads/ThreadRunner.java
@@ -6,40 +6,55 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadRunner implements Runnable{
 
-    private String name;
-    public static volatile AtomicInteger counter = new AtomicInteger(0);
+    private static  AtomicInteger counter = new AtomicInteger(0);       
     
-    public ThreadRunner(String name){
-        this.name = name;
-    }    
-    
-    /*@Override
-    public void run(){
-        while(counter.get() <=50){
-            System.out.println(this.name + "  " + counter.getAndIncrement());
-        }               
-    }*/
-
     @Override
     public void run(){
-        int temp = 0;
-        while(counter.get() <= 50){
-            for(int i = 0; i < 1000000; i++){
-                temp++;
-            }
-            if(counter.get() <= 50)
-                System.out.println(this.name + " " + counter.getAndIncrement());
-            for(int i = 0; i < 1000000; i++){
-                temp--;
-            }
-        }
+        
+        /*
+         * verzia 1. nepotrebujeme v poradi
+         */
+        for(;;){
+            int tmp = counter.getAndIncrement();
+            if(tmp > 50) break;
+            System.out.println(Thread.currentThread().getName() + " " + tmp);            
+        }            
     }
+    
     
     public static void main(String[] args){
         for(int i = 0; i < 3; i++){
-            ThreadRunner runner = new ThreadRunner("Thread " + i);
-            Thread thread = new Thread(runner);
+            ThreadRunner runner = new ThreadRunner();
+            Thread thread = new Thread(runner,"Thread" + i);
             thread.start();            
         }        
-    }
+    }  
+    
+    
+        /*
+         * verzia 2. cisla vypisovane v poradi
+         */
+        
+        /*private int counter;
+        
+        @Override
+        public void run(){
+            synchronized(this){
+                for(;;){
+                    if(counter > 50) break;
+                    System.out.println(counter++);
+                }
+            }
+        }
+        
+         
+        public static void main(String ... args){
+           ThreadRunner runner = new ThreadRunner();      
+           for(int i = 0; i < 3; i++){
+              Thread thread = new Thread(runner,"Thread" + i);
+              thread.start();
+           }            
+        }*/
+        
+    
 }
